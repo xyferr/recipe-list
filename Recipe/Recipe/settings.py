28664145310facb899c16d6f01549203882f9326
xyferr <rohit22153105@akgec.ac.in>
2024-04-28 20:44:45 +0530

    new commit

--- Recipe/Recipe/settings.py
+++ Recipe/Recipe/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Recipe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'vege/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
